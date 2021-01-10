5479885cb80a5271f76aee27eed79f50bcad1d53
fjolla halimi <f.halimi1@hotmail.com>
2021-01-10 20:21:32 +0100

    krijimi i index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function validateForm(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username === "" || password === "" ){
+        alert("Te gjitha te dhenat duhet te mbushen!");
+        return false;
+    }
+    // let regex = /^[a-z]{4}[0-9]{9}$/gi;
+    // if(!isbn.match(regex)){
+    //     alert("ISBN duhet ti permbaj 4 shkronja dhe 9 numra");
+    //     return false;
+    // }
+    // let emailRegex = /^[a-z0-9+_.-]+@(.+)$/gi
+    // if(!autori.match(emailRegex)){
+    //     alert("Autori duhet te jete email");
+    //     return false;
+    // }
+    return true;
+}
